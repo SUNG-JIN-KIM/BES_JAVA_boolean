23a04c17f473ab924651c28d75066ed1f48c02aa
SUNG-JIN-KIM <ksjcloud98@gmail.com>
2025-06-04 10:44:44 +0900

    boolean2 Commit

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 20;
+        int a = 10;
+        int b = 7;
 
-        boolean hasID = true;
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
 
-        if(age >= 18 && hasID){
-            System.out.println("You can enter.");
-        }
-
-        boolean isWeekend = false;
-        boolean isHoliday = true;
-
-        if(isWeekend || isHoliday){
-            System.out.println("You can take a break.");
-        }
-
-        boolean isRaining = true;
-
-        if(!isRaining){
-            System.out.println("Just get out.");
-        }else{
-            System.out.println("Bring your umbrella.");
-        }
+        System.out.println("a > b : " + (a > b));
+        System.out.println("a < b : " + (a < b));
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b : " + (a <= b));
     }
 }
